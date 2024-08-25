80ad623d30eaabd0e32dc6eaa8bcdb604aceb0cd
controls player <130595852+site-codes@users.noreply.github.com>
2024-08-25 19:08:02 -0300

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,14 @@ link.type = 'text/css';
 link.rel = 'stylesheet';
 link.href = 'https://site-codes.github.io/Projetosite/geral.css';
 
-document.querySelector('head').appendChild(link);
+document.querySelector('head').appendChild(link)
+
+// Adiciona o segundo arquivo CSS
+let linkmain = document.createElement('link');
+linkmain.type = 'text/css';
+linkmain.rel = 'stylesheet';
+linkmain.href = 'https://site-codes.github.io/Projetosite/geral.css';
+document.querySelector('head').appendChild(linkmain);
       
       
 
