948d9edbafecd3c0e27a4c11c344603ff408c2e6
controls player <130595852+site-codes@users.noreply.github.com>
2024-09-18 16:31:57 -0300

    Update and rename links.js to linkspageanime.js

---
+++ linkspageanime.js
@@ -0,0 +1,12 @@
+// Cria e adiciona o link para o CSS
+var link = document.createElement('link');
+link.href = 'pageanime.css';
+link.rel = 'stylesheet';
+document.head.appendChild(link);
+
+// Cria e adiciona o script para o JavaScript
+var script = document.createElement('script');
+script.src = 'pageanime.js';
+document.head.appendChild(script);
+  
+  
