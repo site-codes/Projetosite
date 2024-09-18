475600277f955e8ed4828be415e73da40660e889
controls player <130595852+site-codes@users.noreply.github.com>
2024-09-18 16:50:25 -0300

    Update linkspageanime.js

--- linkspageanime.js
+++ linkspageanime.js
@@ -1,12 +1,12 @@
 // Cria e adiciona o link para o CSS
 var link = document.createElement('link');
-link.href = '/Projetosite/pageanime.css';
+link.href = 'https://site-codes.github.io/Projetosite/pageanime.css';
 link.rel = 'stylesheet';
 document.head.appendChild(link);
 
 // Cria e adiciona o script para o JavaScript
 var script = document.createElement('script');
-script.src = '/Projetosite/pageanime.js';
+script.src = 'https://site-codes.github.io/Projetosite/pageanime.js';
 document.head.appendChild(script);
   
   
