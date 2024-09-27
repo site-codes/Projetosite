8dfaaed5affa81add6f9204e23cc672d86a160af
controls player <130595852+site-codes@users.noreply.github.com>
2024-09-27 20:10:17 -0300

    Update script.js

--- Player/script.js
+++ Player/script.js
@@ -2,7 +2,7 @@
 
 // Adicionar o script final
 const scriptJs = document.createElement('script');
-scriptJs.src = '/Player/scriptFinalVersion.js';
+scriptJs.src = '/Projetosite/Player/scriptFinalVersion.js';
 document.body.appendChild(scriptJs);
 
 // Adicionar o link para a fonte do Google
