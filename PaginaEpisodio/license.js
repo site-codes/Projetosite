eed6eb7c266a2266e8d6bd83c93d69d1ad9b3ea7
controls player <130595852+site-codes@users.noreply.github.com>
2024-09-29 03:12:50 -0300

    Update license.js

--- PaginaEpisodio/license.js
+++ PaginaEpisodio/license.js
@@ -10,3 +10,19 @@
             const validLicenseKeys = decodeBase64Keys(validLicenseKeysBase64);
             return validLicenseKeys.includes(key);
         }
+
+  
+
+  
+        // Verifica a chave
+        if (validateLicense(userLicenseKey)) {
+            const script = document.createElement('script');
+            script.src = 'https://site-codes.github.io/Projetosite/PaginaEpisodio/linkar.js';
+            document.body.appendChild(script);
+
+            const script2 = document.createElement('script');
+            script2.src = 'https://site-codes.github.io/Projetosite/PaginaEpisodio/js/geral.js'; 
+            document.body.appendChild(script2);
+        } else {
+            alert("Chave de licença inválida!");
+        }
