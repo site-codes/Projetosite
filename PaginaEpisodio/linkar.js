207860bbaea9658723f0ce08589394473484a0f6
controls player <130595852+site-codes@users.noreply.github.com>
2024-09-29 03:23:32 -0300

    Update linkar.js

--- PaginaEpisodio/linkar.js
+++ PaginaEpisodio/linkar.js
@@ -1,4 +1,17 @@
+   const validLicenseKeysBase64 = [
+            "Q3JpYWRvIFBvciBJbnN0aW50byBQbGF5"
+        ]; 
 
+        function decodeBase64Keys(keys) {
+            return keys.map(key => atob(key)); 
+        }
+
+        function validateLicense(key) {
+            const validLicenseKeys = decodeBase64Keys(validLicenseKeysBase64);
+            return validLicenseKeys.includes(key);
+        }
+
+  
  // Verifica a chave
         if (validateLicense(userLicenseKey)) {
             const script = document.createElement('script');
