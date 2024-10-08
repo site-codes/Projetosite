dc59d2d58358e07088ad254af10753bcff4c12ae
controls player <130595852+site-codes@users.noreply.github.com>
2024-10-07 21:57:28 -0300

    Add files via upload

---
+++ PaginaAnime/linkspageanime.js
@@ -0,0 +1,15 @@
+   // Cria e adiciona o link para o CSS
+   var link = document.createElement('link');
+   link.href = 'https://site-codes.github.io/Projetosite/pageanime.css';
+   link.rel = 'stylesheet';
+   document.head.appendChild(link);
+   
+   // Cria e adiciona o script para o JavaScript
+   var script = document.createElement('script');
+   script.src = 'https://site-codes.github.io/Projetosite/pageanime.js';
+   document.head.appendChild(script);
+     
+
+   // senha *Matheus2*
+     
+   
