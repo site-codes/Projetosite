4801809f15032116f03bb6192803e6f25513fba8
controls player <130595852+site-codes@users.noreply.github.com>
2024-10-09 15:43:31 -0300

    Update script.js

--- PaginaAnime/script.js
+++ PaginaAnime/script.js
@@ -67,26 +67,6 @@ function altcapa(event) {
   checkTrailerLink();
   
 
-    // SUBSTITUIR TERMOS
-const sinopseElements = document.querySelectorAll('.sinopseN');
-const description = document.getElementById('description');
-
-if (description) {
-sinopseElements.forEach(sinopse => {
-  if (sinopse.innerHTML.trim() === '') {
-    sinopse.innerHTML = description.innerHTML;
-  }
-});
-}
-    const tituloElements = document.querySelectorAll('.tituloN');
-
-tituloElements.forEach(titulo => {
-if (!titulo.getAttribute('data-name').trim()) {
-  titulo.setAttribute('data-name', 'Sem Título');
-}
-});
-
-
 
 
   // DISPLAY NONE AO ADM CASO VALOR VAZIO
