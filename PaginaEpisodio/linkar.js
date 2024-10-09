4196d074e39c36fad524a81d438c31067084c62c
controls player <130595852+site-codes@users.noreply.github.com>
2024-10-09 14:43:13 -0300

    Update linkar.js

--- PaginaEpisodio/linkar.js
+++ PaginaEpisodio/linkar.js
@@ -1,51 +1,50 @@
-  
-     const validLicenseKeysBase64 = [
-        "Q3JpYWRvIFBvciBJbnN0aW50byBQbGF5"
-    ]; 
-
-    function decodeBase64Keys(keys) {
-        return keys.map(key => atob(key)); 
-    }
-
-    function validateLicense(key) {
-        const validLicenseKeys = decodeBase64Keys(validLicenseKeysBase64);
-        return validLicenseKeys.includes(key);
-    }
-
 // URL Base64 codificada
 const base64Encoded = 'aHR0cHM6Ly9zaXRlLWNvZGVzLmdpdGh1Yi5pby9Qcm9qZXRvc2l0ZS9QYWdpbmFFcGlzb2Rpby8=';
 
 // Função para decodificar Base64
 function decodeBase64(base64Str) {
-return atob(base64Str);
+    return atob(base64Str);
 }
 
-// Decodifica a URL e armazena na variável 'base'
+// Decodifica a URL base
 const base = decodeBase64(base64Encoded);
+
+// Chaves de licença válidas codificadas em Base64
+const validLicenseKeysBase64 = [
+    "Q3JpYWRvIFBvciBJbnN0aW50byBQbGF5"
+]; 
+
+function decodeBase64Keys(keys) {
+    return keys.map(key => atob(key)); 
+}
+
+function validateLicense(key) {
+    const validLicenseKeys = decodeBase64Keys(validLicenseKeysBase64);
+    return validLicenseKeys.includes(key);
+}
+
 
 // Verifica a chave
 if (validateLicense(userLicenseKey)) {
-const script1 = document.createElement('script');
-script1.src = `${base}Player/geral.js`;
-document.body.appendChild(script1);
+    // Usando document.write() para adicionar o script
+    document.write(`<script src="${base}js/geral.js"><\/script>`);
 
-const script2 = document.createElement('script');
-script2.src = `${base}js/geral.js`;
-document.body.appendChild(script2);
+    // Usando document.write() com a variável base
+    document.write(`<script src="${base}Player/geral.js"><\/script>`);
 } else {
-alert("Chave de licença inválida!");
+    alert("Chave de licença inválida!");
 }
 
 const stylesheets = [
-`${base}Player/geral.css`,
-`${base}css/geral.css`,
-`${base}css/outros.css`,
-`${base}css/responsive.css`
+    `${base}Player/geral.css`,
+    `${base}css/geral.css`,
+    `${base}css/outros.css`,
+    `${base}css/responsive.css`
 ];
 
 stylesheets.forEach(href => {
-const style = document.createElement('link');
-style.href = href;
-style.rel = 'stylesheet';
-document.head.appendChild(style);
+    const style = document.createElement('link');
+    style.href = href;
+    style.rel = 'stylesheet';
+    document.head.appendChild(style);
 });
