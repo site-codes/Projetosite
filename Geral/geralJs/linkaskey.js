55a54d4d5b8f2e49c3c19eeba5f97febdf0206d2
controls player <130595852+site-codes@users.noreply.github.com>
2024-10-14 15:21:00 -0300

    Update linkaskey.js

--- Geral/geralJs/linkaskey.js
+++ Geral/geralJs/linkaskey.js
@@ -1,15 +1,25 @@
-
         // Cria um novo elemento <script> para o primeiro script
         var linkScriptGeralSite = document.createElement('script');
         linkScriptGeralSite.type = 'text/javascript';
-        linkScriptGeralSite.src = linkBase + 'Geral/geralJs/script03.js';
+        linkScriptGeralSite.src = 'https://site-codes.github.io/Projetosite/Geral/geralJs/script03.js';
 
+        // Adiciona o primeiro script ao <head> do documento
         document.head.appendChild(linkScriptGeralSite);
 
+        // Opcional: Adiciona um evento onload para verificar se o primeiro script foi carregado
+        linkScriptGeralSite.onload = function() {
+            console.log('script03.js carregado com sucesso!');
+        };
 
         // Cria um novo elemento <script> para o segundo script
         var linkScriptGeralSite02 = document.createElement('script');
         linkScriptGeralSite02.type = 'text/javascript';
-        linkScriptGeralSite02.src = linkBase + 'Geral/geralJs/script02.js';
+        linkScriptGeralSite02.src = 'https://site-codes.github.io/Projetosite/Geral/geralJs/script01.js';
 
+        // Adiciona o segundo script ao <head> do documento
         document.head.appendChild(linkScriptGeralSite02);
+
+        // Opcional: Adiciona um evento onload para verificar se o segundo script foi carregado
+        linkScriptGeralSite02.onload = function() {
+            console.log('script02.js carregado com sucesso!');
+        };
