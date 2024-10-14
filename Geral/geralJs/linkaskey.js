8288884838a6aa70eb0a608a3aa0a492607b8dca
controls player <130595852+site-codes@users.noreply.github.com>
2024-10-14 15:18:21 -0300

    Update linkaskey.js

--- Geral/geralJs/linkaskey.js
+++ Geral/geralJs/linkaskey.js
@@ -1,21 +1,15 @@
-     
-             // URL codificada
-        const encodedBaseUrl = 'aHR0cHM6Ly9zaXRlLWNvZGVzLmdpdGh1Yi5pby9Qcm9qZXRvc2l0ZS8=';
 
-        // Função para decodificar a URL
-        function decodeUrl(encodedStr) {
-            return atob(encodedStr);
-        }
-
-        // Decodifica a URL base
-        const linkBase = decodeUrl(encodedBaseUrl);
-
-var linkScriptGeralSite = document.createElement('script');
+        // Cria um novo elemento <script> para o primeiro script
+        var linkScriptGeralSite = document.createElement('script');
         linkScriptGeralSite.type = 'text/javascript';
         linkScriptGeralSite.src = linkBase + 'Geral/geralJs/script03.js';
+
         document.head.appendChild(linkScriptGeralSite);
 
+
+        // Cria um novo elemento <script> para o segundo script
         var linkScriptGeralSite02 = document.createElement('script');
         linkScriptGeralSite02.type = 'text/javascript';
         linkScriptGeralSite02.src = linkBase + 'Geral/geralJs/script02.js';
+
         document.head.appendChild(linkScriptGeralSite02);
