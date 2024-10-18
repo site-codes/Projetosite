5dd3fd78724696818b7a46ed06ffbf8123131437
controls player <130595852+site-codes@users.noreply.github.com>
2024-10-18 19:16:44 -0300

    Update linksKey.js

--- Geral/geralJs/linksKey.js
+++ Geral/geralJs/linksKey.js
@@ -1,5 +1,7 @@
-document.write('<script src="https://site-codes.github.io/Projetosite/Geral/geralJs/script01.js"><\/script>');
-document.write('<script src="https://site-codes.github.io/Projetosite/Geral/geralJs/script02.js"><\/script>');
-document.write('<script src="https://site-codes.github.io/Projetosite/Geral/geralJs/script03.js"><\/script>');
+
+ document.write('<script src="' + linkBase + 'Geral/geralJs/script01.js"><\/script>');
+ document.write('<script src="' + linkBase + 'Geral/geralJs/script02.js"><\/script>');
+ document.write('<script src="' + linkBase + 'Geral/geralJs/script03.js"><\/script>');
+ document.write('<script src="' + linkBase + 'Geral/geralJs/script04.js"><\/script>');
 
 document.write('<link rel="stylesheet" type="text/css" href="https://site-codes.github.io/Projetosite/Geral/geral.css">');
