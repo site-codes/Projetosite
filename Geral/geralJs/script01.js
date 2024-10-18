54e979ed7840676bd4678b58daa419487348668e
controls player <130595852+site-codes@users.noreply.github.com>
2024-10-18 18:35:43 -0300

    Update script01.js

--- Geral/geralJs/script01.js
+++ Geral/geralJs/script01.js
@@ -1,3 +1,275 @@
+  //PERFIL COMENTARIOS BLOGGER ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+const imageUrls = [
+    "https://animangeek.com/wp-content/uploads/2023/08/Os-10-protagonistas-de-anime-mais-gentis-que-estao-sempre-alegres.webp",
+    "https://sm.ign.com/t/ign_br/screenshot/default/imagem-2023-12-15-163503393_5d5p.960.jpg",
+    "https://sm.ign.com/t/ign_br/screenshot/default/imagem-2023-12-15-163612387_sch3.960.jpg",
+    "https://sm.ign.com/t/ign_br/screenshot/default/imagem-2023-12-15-163554237_tmjq.960.jpg",
+    "https://conteudo.imguol.com.br/c/entretenimento/be/2024/04/09/cena-do-anime-wind-breaker-1712689874349_v2_900x506.png.webp",
+    "https://s2-g1.glbimg.com/I4BAAwl--JXhtl6h6A4qDnRw6Vk=/0x0:1280x720/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2024/C/4/9EhaAnTGOvbBj66iUBSQ/chainsaw3.jpg",
+    "https://s2-techtudo.glbimg.com/0CuxZpaH6Mg_0864-mQCEPpj_h4=/0x0:1200x700/1000x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2022/2/D/bVe5uMQYWpxlLaBY9oCA/chainsaw-man-2.jpg",
+    "https://a.storyblok.com/f/178900/1200x675/a658f30530/you-are-ms-servant.jpg/m/1200x0/filters:quality(95)format(webp)",
+    "https://imgix.ranker.com/list_img_v2/14711/2814711/original/anime-characters-died-multiple-times?auto=format&q=50&fit=crop&fm=pjpg&dpr=2&crop=faces&h=356.020942408377&w=680",
+];
+
+const listItems = document.querySelectorAll(".cmHl li");
+
+const style = document.createElement("style");
+style.type = "text/css";
+let cssRules = "";
+
+listItems.forEach((item, index) => {
+    const imageIndex = index % imageUrls.length; 
+    const rule = `.cmHl li:nth-child(${index + 1}) .cmAv .im:before { 
+        content: ""; 
+        background-image: url(${imageUrls[imageIndex]}); 
+        background-repeat: no-repeat; 
+        background-position: center; 
+        background-size: cover; 
+        top: 0; 
+        left: 0; 
+        width: 100%; 
+        height: 100%; 
+        position: absolute; 
+    }\n`;
+    cssRules += rule; 
+});
+
+style.innerHTML = cssRules;
+document.head.appendChild(style);
+
+
+
+// LABELS ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+function addBorders() {
+  document.querySelectorAll('.ntry').forEach(function(element) {
+     
+      const hasAnimesLink = element.querySelector('a[data-text="Animes"], span[data-text="Animes"]');
+ 
+      if (hasAnimesLink) {  
+          element.classList.add('animes');         
+      } else {             
+          element.classList.remove('animes');    
+      }
+ 
+      element.querySelectorAll('a[data-text="Anime Legendado"], span[data-text="Anime Legendado"], a[data-text="Anime Dublado"], span[data-text="Anime Dublado"]').forEach(function(link) {
+          if (link.getAttribute('data-text') === "Anime Legendado") {
+              link.classList.add('sub');            
+              link.setAttribute('data-text', 'Anime Legendado');  
+              link.textContent = 'Leg';                
+          } else if (link.getAttribute('data-text') === "Anime Dublado") {
+              link.setAttribute('data-text', 'Anime Dublado');     
+              link.textContent = 'Dub';                  
+          }
+      });
+ 
+      element.querySelectorAll('a[data-text="Filme Legendado"], span[data-text="Filme Legendado"], a[data-text="Filme Dublado"], span[data-text="Filme Dublado"]').forEach(function(link) {
+          if (link.getAttribute('data-text') === "Filme Legendado") {
+              link.classList.add('sub');               
+              link.setAttribute('data-text', 'Filme Legendado');   
+              link.textContent = 'Leg';                 
+          } else if (link.getAttribute('data-text') === "Filme Dublado") {
+              link.setAttribute('data-text', 'Filme Dublado');      
+              link.textContent = 'Dub';                   
+          }
+      });
+ 
+      element.querySelectorAll('a[data-text="Filmes"], span[data-text="Filmes"]').forEach(function(link) {
+          link.classList.add('labelFilmes');          
+          link.textContent = 'Filme';    
+          element.classList.add('filmes');             
+      });
+ 
+      element.querySelectorAll('a[data-text], span[data-text]').forEach(function(link) {
+          const notaMatch = link.getAttribute('data-text').match(/Nota\s+([\d]+(\.[\d]+)?)/);
+          if (notaMatch) {
+              link.classList.add('labelNota');          
+              link.textContent = notaMatch[1];        
+          }
+ 
+          const temporadasMatch = link.getAttribute('data-text').match(/(\d+)\s+Temporadas/);
+          if (temporadasMatch) {
+              link.classList.add('labelT');   
+              const numeroTemporadas = String(temporadasMatch[1]).padStart(2, '0');
+              link.textContent = numeroTemporadas;   
+          }
+ 
+          const episodiosMatch = link.getAttribute('data-text').match(/(\d+)\s+Episodios/);
+          if (episodiosMatch) {
+              link.classList.add('labelE');                
+              link.textContent = episodiosMatch[1];     
+          }
+      });
+  });
+ }
+ 
+ addBorders();
+ 
+ const observer = new MutationObserver(function(mutationsList, observer) {
+  addBorders();  
+ });
+ 
+ observer.observe(document.body, {
+  childList: true, 
+  subtree: true 
+ });
+
+ 
+// LISTA DE A - Z ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+document.addEventListener('DOMContentLoaded', function () {
+  var letras = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ#';
+  var listaLetras = document.querySelector('.listaLetras');
+  var animeItems = document.querySelectorAll('#Blog1 .blogPts .animes');
+  var filmeItems = document.querySelectorAll('#Blog1 .blogPts .filmes');
+  var searchText = document.getElementById('searchText');
+  var noResultsMessage = document.getElementById('noResultsMessage');
+
+  letras.split('').forEach(function (letra) {
+    var button = document.createElement('button');
+    button.textContent = letra;
+    button.classList.add('letra');
+    button.setAttribute('data-letra', letra);
+    listaLetras.appendChild(button);
+
+    button.addEventListener('click', function () {
+      var letraSelecionada = this.getAttribute('data-letra');
+
+      if (this.classList.contains('letterS')) {
+        this.classList.remove('letterS');
+
+        animeItems.forEach(function (item) {
+          item.style.display = 'block';
+        });
+        filmeItems.forEach(function (item) {
+          item.style.display = 'block';
+        });
+
+        searchText.style.pointerEvents = 'auto';
+
+        verificarResultados();
+      } else {
+        document.querySelectorAll('.listaLetras .letra').forEach(function (btn) {
+          btn.classList.remove('letterS');
+        });
+
+        this.classList.add('letterS');
+
+        animeItems.forEach(function (item) {
+          var primeiraLetra = item.querySelector('.titlePos').textContent.trim().charAt(0).toUpperCase();
+          if (letraSelecionada === '#' && !isNaN(primeiraLetra)) {
+            item.style.display = 'block';
+          } else if (primeiraLetra === letraSelecionada) {
+            item.style.display = 'block';
+          } else {
+            item.style.display = 'none';
+          }
+        });
+
+        filmeItems.forEach(function (item) {
+          var primeiraLetra = item.querySelector('.titlePos').textContent.trim().charAt(0).toUpperCase();
+          if (letraSelecionada === '#' && !isNaN(primeiraLetra)) {
+            item.style.display = 'block';
+          } else if (primeiraLetra === letraSelecionada) {
+            item.style.display = 'block';
+          } else {
+            item.style.display = 'none';
+          }
+        });
+
+        searchText.style.pointerEvents = 'none';
+
+        verificarResultados();
+      }
+    });
+  });
+
+  document.getElementById('searchText').addEventListener('input', function () {
+    var searchTerm = this.value.trim().toLowerCase();
+    var letraSelecionada = document.querySelector('.listaLetras .letra.letterS');
+
+    animeItems.forEach(function (item) {
+      var titlePos = item.querySelector('.titlePos').textContent.trim().toLowerCase();
+      var primeiraLetra = item.querySelector('.titlePos').textContent.trim().charAt(0).toUpperCase();
+
+      if (letraSelecionada) {
+        if (letraSelecionada.textContent === '#' && !isNaN(primeiraLetra)) {
+          if (titlePos.includes(searchTerm)) {
+            item.style.display = 'unset';
+          } else {
+            item.style.display = 'none';
+          }
+        } else if (primeiraLetra === letraSelecionada.textContent) {
+          if (titlePos.includes(searchTerm)) {
+            item.style.display = 'unset';
+          } else {
+            item.style.display = 'none';
+          }
+        } else {
+          item.style.display = 'none';
+        }
+      } else {
+        if (titlePos.includes(searchTerm)) {
+          item.style.display = 'unset';
+        } else {
+          item.style.display = 'none';
+        }
+      }
+    });
+
+    filmeItems.forEach(function (item) {
+      var titlePos = item.querySelector('.titlePos').textContent.trim().toLowerCase();
+      var primeiraLetra = item.querySelector('.titlePos').textContent.trim().charAt(0).toUpperCase();
+
+      if (letraSelecionada) {
+        if (letraSelecionada.textContent === '#' && !isNaN(primeiraLetra)) {
+          if (titlePos.includes(searchTerm)) {
+            item.style.display = 'unset';
+          } else {
+            item.style.display = 'none';
+          }
+        } else if (primeiraLetra === letraSelecionada.textContent) {
+          if (titlePos.includes(searchTerm)) {
+            item.style.display = 'unset';
+          } else {
+            item.style.display = 'none';
+          }
+        } else {
+          item.style.display = 'none';
+        }
+      } else {
+        if (titlePos.includes(searchTerm)) {
+          item.style.display = 'unset';
+        } else {
+          item.style.display = 'none';
+        }
+      }
+    });
+
+    verificarResultados();
+  });
+
+  function verificarResultados() {
+    var hasResults = false;
+
+    animeItems.forEach(function (item) {
+      if (item.style.display !== 'none') {
+        hasResults = true;
+      }
+    });
+
+    filmeItems.forEach(function (item) {
+      if (item.style.display !== 'none') {
+        hasResults = true;
+      }
+    });
+
+    if (hasResults) {
+      noResultsMessage.style.display = 'none';
+    } else {
+      noResultsMessage.style.display = 'flex';
+    }
+  }
+});
 
 
 // CALENDARIO ABRIR  ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -12,7 +284,9 @@ document.addEventListener('DOMContentLoaded', function() {
     }
    });
    
-   // BLOG1 EM LABEL DE TEMPORADAS ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+// BLOG1 EM LABEL DE TEMPORADAS ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
       // 
       var searchElement = document.querySelector('.srch');
       
@@ -26,4 +300,89 @@ document.addEventListener('DOMContentLoaded', function() {
       if (matchTemporadas) {
           searchElement.classList.add('seasonslist');
       }
-   
+
+
+// THEME MODELO ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+ function applyTheme(theme) {
+     const root = document.documentElement;
+     const themeOptions = document.querySelectorAll('.theme-option');
+     const htmlS = document.querySelector('.htmlS'); // Seleciona o elemento .htmlS
+
+     // Remove a classe .selected de todos os temas
+     themeOptions.forEach(option => option.classList.remove('selected'));
+
+     // Adiciona a classe .selected ao tema selecionado
+     document.getElementById(theme).classList.add('selected');
+     
+     // Remove classes de tema anteriores do elemento .htmlS
+     htmlS.classList.remove('tema1', 'tema2', 'tema3');
+
+     // Adiciona a classe correspondente ao tema selecionado
+     if (theme === "tema1") {
+         root.style.removeProperty("--headC");
+         root.style.removeProperty("--bodyD");
+         root.style.removeProperty("--bodyB");
+         root.style.removeProperty("--linkC");
+         root.style.removeProperty("--linkB");
+         root.style.removeProperty("--linkA");
+         root.style.removeProperty("--linkD");
+         root.style.removeProperty("--cor1");
+         root.style.removeProperty("--cor2");
+         root.style.removeProperty("--corB1");
+         root.style.removeProperty("--corB2");
+         root.style.removeProperty("--corB3");
+         root.style.removeProperty("--mobileM");
+         root.style.removeProperty("--mobileC");
+         root.style.removeProperty("--mobileD");
+         root.style.removeProperty("--contentB");
+     } else if (theme === "tema2") {
+         root.style.setProperty("--headC", "var(--headCTema2)");
+         root.style.setProperty("--bodyD", "var(--bodyDTema2)");
+         root.style.setProperty("--bodyB", "var(--bodyBTema2)");
+         root.style.setProperty("--linkC", "var(--linkCTema2)");
+         root.style.setProperty("--linkB", "var(--linkBTema2)");
+         root.style.setProperty("--linkA", "var(--linkATema2)");
+         root.style.setProperty("--linkD", "var(--linkDTema2)");
+         root.style.setProperty("--cor1", "var(--cor1Tema2)");
+         root.style.setProperty("--cor2", "var(--cor2Tema2)");
+         root.style.setProperty("--corB1", "var(--corB1Tema2)");
+         root.style.setProperty("--corB2", "var(--corB2Tema2)");
+         root.style.setProperty("--corB3", "var(--corB3Tema2)");
+         root.style.setProperty("--mobileM", "var(--mobileMTema2)");
+         root.style.setProperty("--mobileC", "var(--mobileCTema2)");
+         root.style.setProperty("--mobileD", "var(--mobileDTema2)");
+         root.style.setProperty("--contentB", "var(--contentBTema2)");
+         htmlS.classList.add('tema2');
+     } else if (theme === "tema3") {
+         root.style.setProperty("--headC", "var(--headCTema3)");
+         root.style.setProperty("--bodyD", "var(--bodyDTema3)");
+         root.style.setProperty("--bodyB", "var(--bodyBTema3)");
+         root.style.setProperty("--linkC", "var(--linkCTema3)");
+         root.style.setProperty("--linkB", "var(--linkBTema3)");
+         root.style.setProperty("--linkA", "var(--linkATema3)");
+         root.style.setProperty("--linkD", "var(--linkDTema3)");
+         root.style.setProperty("--cor1", "var(--cor1Tema3)");
+         root.style.setProperty("--cor2", "var(--cor2Tema3)");
+         root.style.setProperty("--corB1", "var(--corB1Tema3)");
+         root.style.setProperty("--corB2", "var(--corB2Tema3)");
+         root.style.setProperty("--corB3", "var(--corB3Tema3)");
+         root.style.setProperty("--mobileM", "var(--mobileMTema3)");
+         root.style.setProperty("--mobileC", "var(--mobileCTema3)");
+         root.style.setProperty("--mobileD", "var(--mobileDTema3)");
+         root.style.setProperty("--contentB", "var(--contentBTema3)");
+         htmlS.classList.add('tema3');
+     }
+     
+     // Salva a escolha do tema no localStorage
+     localStorage.setItem('selectedTheme', theme);
+ }
+
+ // Aplica o tema salvo ao carregar a página
+ document.addEventListener("DOMContentLoaded", () => {
+     const savedTheme = localStorage.getItem('selectedTheme');
+     if (savedTheme) {
+         applyTheme(savedTheme);
+     } else {
+         applyTheme('tema1'); // Aplica o tema padrão se nenhum tema estiver salvo
+     }
+ });
